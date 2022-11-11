fb39417fa44c4b01f750ba580dd1e762245fe2a4
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-11-11 17:25:32 -0300

    Update styles.ts

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -39,6 +39,22 @@ export const Content = styled(Dialog.Content)`
             color: ${props => props.theme["gray-500"]};
         }
 
+        button[type="submit"] {
+          height: 58px;
+          border: 0;
+          background: ${props => props.theme["green-500"]};
+          color: ${props => props.theme.white};
+          font-weight: bold;
+          padding: 0 1.25rem;
+          border-radius: 6px;
+          margin-top: 1.5rem;
+          cursor: pointer;
+
+          &::hover {
+            background: ${props => props.theme["green-700"]};
+            transition: background-color 0.2s;
+          }
+        }
     }
   }
 `
