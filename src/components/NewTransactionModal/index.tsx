f2ee2c94581958b12e4959f9edb5e5efcf958881
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-11-11 16:51:47 -0300

    Update index.tsx

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -6,9 +6,17 @@ export function NewTransactionModal() {
         <Dialog.Portal>
             <Overlay/>
                <Content>
-                      <Dialog.Title>
-                        Nova Transação
-                      </Dialog.Title>
+                      <Dialog.Title>Nova Transação</Dialog.Title>
+
+                      <form action="">
+                        <input type="text" placeholder="Descrição" required/>
+                        <input type="number" placeholder="Preço" required/>
+                        <input type="text" placeholder="Categoria" required/>
+
+                        <button type="submit">
+                            Cadastrar
+                        </button>
+                      </form>
                       <Dialog.Close/>
                </Content>
         </Dialog.Portal>
