e9bff6052685ac8fd88c35bd081b0ae310f20da7
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-11-15 10:32:20 -0300

    Update TransactionsContext.tsx

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -19,7 +19,7 @@ interface TransactionsProviderProps {
     children: ReactNode;
 }
 
-const TransactionContext = createContext({} as TransactionContextType)
+export const TransactionsContext = createContext({} as TransactionContextType)
 
 export function TransactionsProvider({children} : TransactionsProviderProps) {
 
@@ -37,8 +37,8 @@ export function TransactionsProvider({children} : TransactionsProviderProps) {
     
 
     return(
-        <TransactionContext.Provider value={{transactions}}>
+        <TransactionsContext.Provider value={{transactions}}>
             {children}
-        </TransactionContext.Provider>
+        </TransactionsContext.Provider>
     )
 }
