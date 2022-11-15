d0577f7de07297edb00859332c6a92b821a95b0d
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-11-15 10:19:40 -0300

    Create TransactionsContext.tsx

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,20 @@
+import { createContext } from "react"
+
+
+interface Transaction {
+    id: number;
+    description: string;
+    type: 'income' | 'outcome';
+    price: number;
+    category: string;
+    createdAt: string;
+  }
+  
+
+interface TransactionContextType {
+  transactions: Transaction[];
+}
+
+const TransactionContext = createContext<TransactionContextType>({
+    transactions: []
+})
