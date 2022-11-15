365472d255ebd6dddaa1502d3e9ae8d023c6993f
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-11-15 10:48:32 -0300

    Update index.tsx

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,7 +6,22 @@ import { SummaryCard, SummaryContainer } from "./styles";
 export function Summary() {
 
   const {transactions} = useContext(TransactionsContext)
-    console.log(transactions)
+  
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+       if(transaction.type === 'income') {
+        acc.income += transaction.price;
+       } else {
+        acc.outcome += transaction.price;
+       }
+
+       return acc;
+    },
+    {
+        income: 0,
+        outcome: 0,
+        total: 0
+    })
 
     return(
         <SummaryContainer>
