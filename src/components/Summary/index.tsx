cb5f902722992653e441ba39f27464bcc7dff917
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-11-15 10:52:28 -0300

    Update index.tsx

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -32,21 +32,21 @@ export function Summary() {
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00b37e"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.income}</strong>
             </SummaryCard>
             <SummaryCard>
                 <header>
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color="#f75a68"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.outcome}</strong>
             </SummaryCard>
             <SummaryCard variant="green">
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#fff"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
