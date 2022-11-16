43a062dc5fe7d01b9a89ca23d6048ce93b3abe91
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-11-16 10:17:24 -0300

    Update index.tsx

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,28 @@
 import { SerachFormContainer } from "./styles"
 import {MagnifyingGlass} from "phosphor-react"
 import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers"
+
+
+const searchFormSchema = z.object({
+    query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
 
 export function SearchForm() {
-  const { register, handleSubmit} = useForm()
+  const { register, handleSubmit} = useForm<SearchFormInputs>({
+     resolver: zodResolver(searchFormSchema),
+  })
+
+  function handleSearchTransactions(data: SearchFormInputs) {
+     console.log(data)
+  }//functions disparadas por users, começam com 'handle'
 
     return(
-        <SerachFormContainer>
+        <SerachFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
             <input 
              type="text" 
              placeholder="Busque por transações" 
