c4a893082f7f90d437b25d6182be8ea09cae74dc
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-11-16 09:34:06 -0300

    Update index.tsx

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -26,6 +26,7 @@ export function Transactions() {
                       <td width="50%">{transaction.description}</td>
                        <td>
                          <PriceHighlight variant={transaction.type}>
+                          {transaction.type === 'outcome' && '- '}
                           {priceFormatter.format(transaction.price)}
                         </PriceHighlight>
                       </td>
