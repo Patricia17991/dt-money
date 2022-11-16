7b0c0bf4b1f85692a8d202f730c6dd1f9df80c83
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-11-16 10:05:33 -0300

    Update index.tsx

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,10 +1,17 @@
 import { SerachFormContainer } from "./styles"
 import {MagnifyingGlass} from "phosphor-react"
+import { useForm } from "react-hook-form"
 
 export function SearchForm() {
+  const { register, handleSubmit} = useForm()
+
     return(
         <SerachFormContainer>
-            <input type="text" placeholder="Busque por transações" />
+            <input 
+             type="text" 
+             placeholder="Busque por transações" 
+             {...register('query')}
+            />
 
             <button type="submit">
                 <MagnifyingGlass size={20}/>
