1434f7ca8da4834b3bf68f8a0b9c4ca8e31f6232
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-11-16 09:30:39 -0300

    Update index.tsx

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -25,7 +26,7 @@ export function Transactions() {
                       <td width="50%">{transaction.description}</td>
                        <td>
                          <PriceHighlight variant={transaction.type}>
-                          {transaction.price}
+                          {priceFormatter.format(transaction.price)}
                         </PriceHighlight>
                       </td>
                        <td>{transaction.category}</td>
